774713f6338fbe93d69372b9bb600dc87898bd07
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2025-12-12 20:42:33 +0600

    scholer details edit

--- src/Pages/Scholarshipdetails/Scholarshipdetails.jsx
+++ src/Pages/Scholarshipdetails/Scholarshipdetails.jsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router";
-import axios from "axios";
+import { useParams, Link, useNavigate } from "react-router";
 import { FaStar } from "react-icons/fa";
 import useAxios from "../../Hooks/useAxios";
+import toast from "react-hot-toast";
 
 export default function ScholarshipDetails() {
   const { id } = useParams();
-  const navigate = useNavigate();
   const [scholarship, setScholarship] = useState(null);
   const [reviews, setReviews] = useState([]);
-
   const axiosInstance = useAxios();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axiosInstance
@@ -24,15 +23,35 @@ export default function ScholarshipDetails() {
       .catch((err) => console.log(err));
   }, [id]);
 
-  console.log(reviews);
-
   if (!scholarship) return <p className="text-center mt-10">Loading...</p>;
+
+  // Apply button handler
+  const handleApply = async () => {
+    const applicationData = {
+      scholarshipId: id,
+      universityName: scholarship.universityName,
+      universityAddress: scholarship.location,
+      feedback: "No feedback yet",
+      subjectCategory: scholarship.subjectCategory, 
+      applicationFees: scholarship.applicationFees,
+      applicationStatus: "Pending",
+    };
+
+    try {
+      await axiosInstance.post("/applications", applicationData);
+      toast.success("Application submitted successfull. Now do Payment");
+      navigate(`/payment/${id}`);
+    } catch (error) {
+      toast.error("Error submitting application");
+    }
+  };
 
   return (
     <div className="max-w-6xl mx-auto mt-17 p-4">
       <h1 className="text-3xl font-bold mb-2 text-center py-6">
         {scholarship.scholarshipName}
       </h1>
+
       <div className="flex flex-col md:flex-row gap-6 bg-white shadow-lg rounded-lg p-6">
         <img
           src={scholarship.universityImage}
@@ -56,12 +75,13 @@ export default function ScholarshipDetails() {
           </p>
           <p className="text-gray-500 mb-3">Stipend: {scholarship.stipend}</p>
           <p className="mb-4">{scholarship.description}</p>
-          <Link
-            to={`/payment/${id}`}
+
+          <button
+            onClick={handleApply}
             className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition"
           >
             Apply for Scholarship
-          </Link>
+          </button>
         </div>
       </div>
 
