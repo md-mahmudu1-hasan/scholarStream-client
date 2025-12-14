7a7c1ef12b5fdbb005cecbde6d0475607a354049
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2025-12-14 15:59:56 +0600

    payment canceled page design

--- src/Pages/PaymentCanceled/PaymentCanceled.jsx
+++ src/Pages/PaymentCanceled/PaymentCanceled.jsx
@@ -1,30 +1,45 @@
-import { useLocation, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
+import useAxios from "../../Hooks/useAxios";
 
 const PaymentCanceled = () => {
-  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
   const navigate = useNavigate();
+  const [paymentData, setPaymentData] = useState(null);
+  const axiosInstance = useAxios();
 
-  const query = new URLSearchParams(location.search);
-  const scholarshipName =
-    query.get("scholarshipName") || "Scholarship Application";
-  const errorMessage =
-    query.get("error") ||
-    "Your payment was canceled or failed. You can try again anytime.";
+  useEffect(() => {
+    if (sessionId) {
+      axiosInstance
+        .get(`/applications/${sessionId}`)
+        .then((res) => {
+          setPaymentData(res.data[0]);
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [sessionId, axiosInstance]);
+
+  if (!paymentData) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-lg font-semibold">Loading payment details...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-50 px-4">
       <div className="bg-white shadow-lg rounded-xl p-8 max-w-lg w-full text-center">
-        <h2 className="text-3xl font-bold text-red-600 mb-4">
-          Payment Failed
-        </h2>
+        <h2 className="text-3xl font-bold text-red-600 mb-4">Payment Failed</h2>
 
         <p className="text-gray-700 mb-2">
           Scholarship:{" "}
-          <span className="font-semibold">{scholarshipName}</span>
+          <span className="font-semibold">{paymentData.ScholarshipName}</span>
         </p>
 
         <p className="text-red-500 mb-6">
-          {errorMessage}
+          Your payment was canceled or failed. You can try again anytime.
         </p>
 
         <button
