eba560887dbfa91664fcee5bc04ed0d5e45d26ca
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2025-12-15 11:14:16 +0600

    jwt token add

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,12 +1,43 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
-  
+  baseURL: "http://localhost:3000/",
 });
 
 const useAxios = () => {
-    return axiosInstance;
-}
+  const { user, SignOut } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const reInterceptor = axiosInstance.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
 
-export default useAxios;  
+    const resInterceptor = axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          SignOut().then(() => {
+            navigate("/login");
+          });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosInstance.interceptors.request.eject(reInterceptor);
+      axiosInstance.interceptors.response.eject(resInterceptor);
+    };
+  }, [user]);
+  return axiosInstance;
+};
+
+export default useAxios;
